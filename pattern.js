7c4c8fe55c701852273ee1ac9196f9750b4e2e7f
rajashri <kadukarrajashri@gmail.com>
2022-02-13 16:37:08 +0530

    added temprature converter code

---
+++ pattern.js
@@ -0,0 +1,11 @@
+
+
+var cel=document.getElementById("c");
+var feh=document.getElementById('f');
+
+cel.addEventListener('input',function(){
+	console.log("cel");
+});
+feh.addEventListener('input',function(){
+	console.log("feh");
+});
